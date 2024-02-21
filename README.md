4d3dab685602e5abae6873d5949b926bd4c5cab4
Arahman75 <selfarahman01825@gmail.com>
2024-02-21 20:18:57 +0600

    added the live link

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# baby-toy-store-server-site" 
+## ## Live site link : https://baby-toy-store-client-abdur-rahmans-projects.vercel.app
+## server site link :https://github.com/Arahman75/baby-toy-store-server-site
--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const port = process.env.PORT || 5000;
 
 
 // middleware
-const corsConfig = {
-    origin: [
-        'http://localhost:5173',
-        'https://baby-toy-store-740bb.web.app',
-        'https://baby-toy-store-740bb.firebaseapp.com'
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig));
-// app.use(cors());
+// const corsConfig = {
+//     origin: [
+//         'http://localhost:5173',
+//         'https://baby-toy-store-740bb.web.app',
+//         'https://baby-toy-store-740bb.firebaseapp.com'
+//     ],
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE']
+// }
+// app.use(cors(corsConfig))
+// app.options("", cors(corsConfig));
+app.use(cors());
 app.use(express.json());
 
 
@@ -36,7 +36,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const productCollection = client.db("babyStore").collection('products');
 
         // get all product
@@ -66,8 +66,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
